cd0fa9c0d695f2ea4406fc2965f543af917fa23c
Dom Jurkus <domantas.jurkus@gmail.com>
2016-11-30 15:01:29 +0000

    Added basic backend

--- js/demo.js
+++ js/demo.js
@@ -1,13 +1,25 @@
 $(document).ready(function() {
     
 	var userIds = getUserIds();
+
 	userIds.forEach(function(id) {
-		$('#user-id-ul').append('<li><a href="#">'+id+'</a></li>');
+		$("#user-id-ul").append('<li><a href="#">'+id+'</a></li>');
 	});
 
 	// TODO: replace with API call
 	function getUserIds() {
 		return [2,3,5,8,];
+	}
+
+	// Data looks like this:
+	var data = {
+		id: 3466,
+		name: "Di Maggio's Airdrie",
+		town: "Airdrie",
+		img: "",
+		PricePoint: 2, // Can be NULL
+		lat: 55.8598369,
+		lng: -3.9994152
 	}
 
 });
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+// app.use(express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
+// app.use('/img', express.static(__dirname + '/img'));
+
+app.get('/', function(req, res) {
+    res.sendFile('views/demo.html', { root: __dirname });
+});
+
+var port = process.env.PORT||3000;
+http.listen(port, function() {
+    console.log('listening on *:3000');
+});
