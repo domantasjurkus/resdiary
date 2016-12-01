85237cc13abfd4bc294a6bfc212670d095a9d768
JosephOHagan <josephaohagan@gmail.com>
2016-12-01 21:56:20 +0000

    Experiments continue - dead end hit now backtracking and going down next branch

--- server.js
+++ server.js
@@ -16,4 +16,20 @@ http.listen(port, function() {
 	console.log('listening on *:3000');
 });
 
+	
+var spawn = require('child_process').spawn,
+	py = spawn('python', ['compute_input.py']),
+    data = [1,2,3,4,5,6,7,8,9],
+    dataString = '';
+
+py.stdout.on('data', function(data){
+  	dataString += data.toString(); 
+});
+
+py.stdout.on('end', function(){
+	console.log('Sum of numbers = HERE =',dataString);
+});
+
+py.stdin.write(JSON.stringify(data));
+py.stdin.end();
 
