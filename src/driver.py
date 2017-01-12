fb8b8aab8a30a4d395fcb0595bfb941b2ce6ad56
Eduardd Kalfov <edward.kalfov@gmail.com>
2017-01-12 14:15:22 +0200

    Example collaborative filtering model

---
+++ src/driver.py
@@ -0,0 +1,29 @@
+from pyspark import SparkContext
+from collaborative_filtering.model import *
+from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
+
+def collaborative_filtering_model(data):
+	rank = 10
+	num_iterations = 10
+	model = ALS.train(data,rank,num_iterations)
+	return model
+
+sc = SparkContext('local', 'Recommendation Engine')
+ratings = sc.parallelize(get_bookings_with_score(read_csv(sc,'/home/edward/Downloads/Booking.csv')))
+data = sc.parallelize(get_data_array(read_csv(sc,'/home/edward/Downloads/Booking.csv')))
+model = collaborative_filtering_model(ratings)
+
+testdata = data.map(lambda p: (p[0], p[2]))
+predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
+print predictions.collect()
+with open('predictions_all.txt','w') as pred_file:
+	for row in predictions.collect():
+		pred_file.write(str(row))
+ratesAndPreds = ratings.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
+MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).mean()
+print("Mean Squared Error = " + str(MSE))
+
+# Save and load model
+# model.save(sc, "tmp/myCollaborativeFilter")
+
+sc.stop()
