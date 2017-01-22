7e22de77a0fb4d148a3fbd9986655b389d173538
Eduard Kalfov <edward.kalfov@gmail.com>
2017-01-22 16:22:04 +0000

    Better control of algorithm execution in main

--- src/main.py
+++ src/main.py
@@ -1,17 +1,32 @@
 import data
+import argparse
 from pyspark import SparkContext
 from evaluator import evaluate
 from recommenders import initial, ALS
 
 sc = SparkContext('local', 'Recommendation Engine')
-bookings = data.filter_outliers(sc, data.read(sc, 'Booking.csv'))
+algorithms = {"als":ALS,"initial":initial}
 
-# data for the front end
-data.write('Recommendations.csv', ALS.generate_recommendations(sc, bookings))
+def get_bookings(filename):
+	bookings = data.filter_outliers(sc, data.read(sc, filename))
+	return bookings
+
+def execute_algorithm(algorithm,filename):
+	bookings = get_bookings(filename)
+	data.write('Recommendations.csv', algorithms[algorithm.lower()].generate_recommendations(sc, bookings))
 
 # evaluation
-for name, algorithm in [('Initial', initial), ('ALS', ALS)]:
-    print '{}: {:.3f}%'.format(
-        name, evaluate(sc, algorithm.generate_recommendations, bookings))
+def evaluate_algorithm(algorithm,filename):
+	bookings = get_bookings(filename)
+	print '{}: {:.3f}%'.format(algorithm, evaluate(sc, algorithms[algorithm.lower()].generate_recommendations, bookings))
 
-sc.stop()
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Executes an algorithm on the selected data. The algorithm is usually a recommendation algorithm.')
+	parser.add_argument('--alg', type=str,help='Algorithm name')
+	parser.add_argument('--data', type=str,help='The data file.')
+	args = parser.parse_args()
+
+	execute_algorithm(args.alg,args.data)
+	evaluate_algorithm(args.alg,args.data)
+	sc.stop()
