44b2ea60c7f501dd7ff0d0c9701cf55e33a6c98a
Eduard Kalfov <edward.kalfov@gmail.com>
2017-01-22 15:45:19 +0000

    Training on 90% of the data and give recommendations for the other 10%.  Rank 12 and lambda=0.1 seems to give more reasonable results.

--- src/recommenders/ALS.py
+++ src/recommenders/ALS.py
@@ -3,7 +3,9 @@ from pyspark.mllib.recommendation import ALS
 
 def generate_recommendations(spark, bookings):
     ratings = get_bookings_with_score(spark, bookings)
-    model = ALS.train(ratings, 10, 10)
-    predictions = model.predictAll(ratings.map(lambda p: (p[0], p[1])))
+    ratings, test_ratings = ratings.randomSplit([0.9,0.1])
+    model = ALS.train(ratings, 12, 10,0.1)
+    testdata = test_ratings.map(lambda p: (p[0], p[1]))
+    predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
     return SQLContext(spark).createDataFrame(
-        predictions.map(lambda r: (r[0], r[1])), ['userID', 'restaurantID'])
+        predictions.map(lambda r: (r[0][0],r[0][1],r[1])), ['userID', 'restaurantID','score'])
