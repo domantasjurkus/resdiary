6bd9a2690fe6dd63c2ddcd65c48081198a45915c
Eduardd Kalfov <edward.kalfov@gmail.com>
2017-02-09 12:08:33 +0000

    Fixed StackOverflow error when the number of training iterations is high. (Closes #120)

--- src/recommenders/ALS_2.py
+++ src/recommenders/ALS_2.py
@@ -1,15 +1,25 @@
 from pyspark.mllib.recommendation import ALS
+from pyspark.sql import SQLContext
 from pyspark.sql.dataframe import DataFrame
+from data import Data
 
-def generate_recommendations(spark, bookings):
+def train_model(spark, bookings):
     if not isinstance(bookings, DataFrame):
         raise TypeError('Recommender requires a DataFrame')
 
     data = Data(spark)
-    ratings = data.get_bookings_with_score(spark, bookings)
-    ratings, test_ratings = ratings.randomSplit([0.99,0.01])
+    ratings = data.get_bookings_with_score( bookings)
+    ratings, test_ratings = ratings.randomSplit([0.8,0.2])
     ratings.cache()
-    model = ALS.train(ratings, 60, 10,1.0)
+    spark.setCheckpointDir("./checkpoints/")
+    model = ALS.train(ratings, 12, 30,0.45)
+    return model
+
+    
+def validate_results(model):
+    data = Data(spark)
+    ratings = data.get_bookings_with_score( bookings)
+    ratings, test_ratings = ratings.randomSplit([0.9,0.1])
     testdata = test_ratings.map(lambda p: (p[0], p[1]))
     # testdata = ratings.map(lambda p: (p[0], p[1]))
     predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
