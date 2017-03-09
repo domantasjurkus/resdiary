22153c8c58c27b47f9559bde7898993220433c12
domantasjurkus <domantas.jurkus@gmail.com>
2017-03-09 13:53:58 +0000

    Small update on ALS Explicit test

---
+++ tests/test_algorithm_als_explicit.py
@@ -0,0 +1,35 @@
+import unittest
+
+from pyspark.mllib.recommendation import MatrixFactorizationModel, ALS
+
+from test_superclass import BaseTestCase
+from src.recommenders import ExplicitALS as algorithm
+from src.data import Data
+
+class ALSExplicitTest(BaseTestCase):
+
+	@classmethod
+	def setUpClass(self):
+
+		# Instantiate alg.model
+		self.alg = algorithm(self.sc)
+
+
+	def test_main(self):	
+		# Check that an exception is thrown if wrong argument provided
+		self.assertRaises(TypeError, self.alg.train, ['foo', 'bar'])
+
+		# Model should only be defined after training
+		self.assertFalse(hasattr(self.alg, 'model'))
+
+		self.alg.train(self.bookings)
+		self.assertTrue(hasattr(self.alg, 'model'))
+		self.assertTrue(type(self.alg.model) in [MatrixFactorizationModel, ALS])
+
+		# Check for detection of empty RDD
+		self.assertRaises(ValueError, self.alg.predict, self.sc.parallelize([]))
+
+
+	@classmethod
+	def tearDownClass(self):
+		del self.alg
